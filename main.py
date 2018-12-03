4f674973ef3b3da59b7291a7fe4229d9981ddae8
csmcallister <scott.mcallister@gsa.gov>
2018-12-03 13:30:44 -0500

    FIX: change boilerplate to anticpate new use

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from db.db import DataAccessLayer
-from db.db_utils import insert_data, fetch_last_update
+from db.db_utils import insert_data
 from skyspark import SkySparkAPI
+from datetime import datetime
 import os
 import sys
 
@@ -11,25 +12,17 @@ if os.getenv('TEST_DB_URL'):
 else:
 	DB_URL = 'sqlite:///:memory:'
 
-if os.getenv('SKYSPARK_KEY'):
-	KEY = os.getenv('SKYSPARK_KEY')
-else:
-    KEY = '123'
-
 dal = DataAccessLayer()
 
 def main():
-    dal.connect()
+    date = datetime.now()
+    ss = SkySparkAPI(date = date)
+    file_name = ss.download_data()
+    df = ss.create_data_frame(file_name)
+    
     session = dal.Session()
-    last_update = fetch_last_update(session)
+    insert_data(df, session)
     session.commit()
-    if not last_update:
-        ss = SkySparkAPI(KEY)
-        grouped_df = ss.json_to_parsed_df()
-        session = dal.Session()
-        insert_data(grouped_df, session)
-        session.commit()
-    else:
-        pass
+
     
    
