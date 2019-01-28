b3a062fa557e12ba6500cedc157824f420ad94bd
csmcallister <scott.mcallister@gsa.gov>
2019-01-28 13:39:10 -0500

    ENH: add dataframe transformation logic

--- skyspark.py
+++ skyspark.py
@@ -48,12 +48,56 @@ class SkySparkAPI:
         
         return file_name
     
+    @staticmethod
+    def _extract_digits(value):
+        '''
+        Extracts digits from the values of a pandas Series
+        '''
+        value_str = str(value)
+        value = "".join(s for s in value_str if s.isdigit() or s == '.')
+        measure = "".join(s for s in value_str if s not in value)
+        if len(value) > 0:
+            value = float(value)
+        else:
+            value = None
+        return value, measure
+    
+    @staticmethod
+    def _parse_col_name(col_name):
+        '''
+        Extracts data from a pandas Series
+        '''
+        col_name = str(col_name)
+        col_name_split = col_name.split("-")
+        building = col_name_split[0]
+        floor = "".join(s for s in col_name_split[1] if s.isdigit())
+        room = col_name_split[2]
+        room_number = "".join(s for s in room if s.isdigit())
+        room_type = "".join(s for s in room if s not in room_number)
+        modality = col_name_split[3]
+        
+        return building, floor, room_type, room_number, modality
+    
     
     def create_data_frame(self, file_name):
+        '''
+        Reads in the csv as a pandas dataframe and then melts it for simpler aggregations
+        '''
         df = pd.read_csv(file_name)
-        #TODO add logic to group the df, arriving at columns for timestamp, building_name, 
-        # location, modality_indicator, unit, value
-        return df
+        value_vars = [x for x in df.columns if x != 'Timestamp']
+        df_melted = pd.melt(df, id_vars = ['Timestamp'], value_vars = value_vars)
+        df_melted['Timestamp'] = pd.to_datetime(df_melted['Timestamp'].str.replace(' Chicago',''))
+        df_melted[['value', 'unit']] = df_melted['value'].apply(SkySparkAPI._extract_digits).apply(pd.Series)
+        df_melted['building'] = 0
+        df_melted['floor'] = 0
+        df_melted['room_type'] = 0
+        df_melted['room_number'] = 0
+        df_melted['modality'] = 0
+        location_cols = df_melted['variable'].apply(SkySparkAPI._parse_col_name).apply(pd.Series)
+        df_melted[['building','floor','room_type','room_number','modality']] = location_cols
+        df_melted = df_melted.drop(labels='variable', axis = 1)
+        
+        return df_melted
 
     
     
